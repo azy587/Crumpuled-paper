1b198454612b68913186786be0c8a538901191d7
azy587 <81004766+azy587@users.noreply.github.com>
2021-06-18 21:32:22 +0530

    Add files via upload

---
+++ project-c25-template-main/paper.js
@@ -0,0 +1,48 @@
+class Paper 
+{
+	constructor(x,y,r)
+	{
+        var options = {
+            isStatic = false,
+            'restitution' : 0.3,
+            'friction': 0,
+            'density' : 1.2
+            
+        }  
+		
+        this.r=r
+		
+		this.image=loadImage("paper.png")
+		this.body=Bodies.circle(x,y,r, options)
+		World.add(world, this.body)
+		
+		
+
+		
+
+	}
+	display()
+	{
+
+			var paperpos=this.body.position;
+	
+			
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+
+		
+			fill(255)
+			
+			
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			imageMode(CENTER)
+			
+			
+	}
+
+}
